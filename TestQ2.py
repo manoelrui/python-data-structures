a618d4351d1ccc87b906edb0aa99e78a698c143c
Rui <manoel.rui@5sdigital.com>
2016-03-24 17:26:48 -0300

    Criado classe SortedLinkedList

---
+++ TestQ2.py
@@ -0,0 +1,61 @@
+import unittest
+from datastructure.list.SortedLinkedList import SortedLinkedList
+
+class TestQ2(unittest.TestCase):
+
+#1. Criar uma lista vazia;
+    def test_creation(self):
+        pass
+
+
+#2. Inserir elemento no inicio;
+    def test_insertion(self):
+        pass
+
+
+#3. Imprimir os valores armazenados na lista;
+    def test_print(self):
+        pass
+
+
+#4 Imprimir os valores armazenados na lista usando recursao;
+    def test_recursive_print(self):
+        pass
+
+
+#5.Imprimir os valores armazenados na lista em ordem reversa (da cauda para a cabeca da lista);
+    def test_print_reverse(self):
+        pass
+
+
+#6.Verificar se a lista esta vazia (retorna 1 se vazia ou 0 se nao vazia);
+    def test_is_empty(self):
+        pass
+
+
+#7.Recuperar/Buscar um determinado elemento da lista;
+    def test_find(self):
+        pass
+
+
+#.8 Remover um determinado elemento da lista;
+    def test_remove(self):
+        pass
+
+
+#9.Remover um determinado elemento da lista usando recursao;
+    def test_remove_recursion(self):
+        pass
+
+
+#10. Liberar a lista;
+    def test_list_clean(self):
+        pass
+
+#11 Verificar se duas lista sao iguais
+    def test_compare_lists(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ datastructure/list/SortedLinkedList.py
@@ -0,0 +1,36 @@
+from List import *
+
+class SortedLinkedList(ABCList):
+    def add(self, value):
+        pass
+
+
+    def find(self, value):
+        pass
+
+
+    def remove(self, value):
+        pass
+
+    def removeRecursive(self, value):
+        pass
+
+
+    def isEmpty(self):
+        pass
+
+
+    def clean(self):
+        pass
+
+
+    def printRecursive(self, node):
+        pass
+
+
+    def printReverse(self, node):
+        pass
+
+
+	def __str__(self):
+			pass
