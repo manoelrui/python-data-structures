d57bf4da3d19131edfdaf8eb81c5181d6bf0a75e
Rui <manoel.rui@5sdigital.com>
2016-03-26 13:48:41 -0300

    Modifiado estrutura para testes

---
+++ test/datastructure/list/run.py
@@ -0,0 +1,16 @@
+from test.list.TestQ1 import *
+from test.list.TestQ2 import *
+from test.list.TestQ4 import *
+from test.list.TestQ5 import *
+
+from test.datastructure.list.TestQ3 import *
+
+if __name__ == '__main__':
+    suite1 = unittest.TestLoader().loadTestsFromTestCase(TestQ1)
+    suite2 = unittest.TestLoader().loadTestsFromTestCase(TestQ2)
+    suite3 = unittest.TestLoader().loadTestsFromTestCase(TestQ3)
+    suite4 = unittest.TestLoader().loadTestsFromTestCase(TestQ4)
+    suite5 = unittest.TestLoader().loadTestsFromTestCase(TestQ5)
+    alltests = unittest.TestSuite([suite1, suite2, suite3, suite4, suite5])
+    runner=unittest.TextTestRunner()
+    runner.run(alltests)
