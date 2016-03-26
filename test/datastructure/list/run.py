71cf64b67f0e0b66e9ffc3503af30bcf71f2d916
Rui <manoel.rui@5sdigital.com>
2016-03-26 14:25:32 -0300

    Atualizado estrutura para testes da lista

--- test/datastructure/list/run.py
+++ test/datastructure/list/run.py
@@ -3,13 +3,13 @@ from test.datastructure.list.TestQ2 import *
 from test.datastructure.list.TestQ4 import *
 from test.datastructure.list.TestQ5 import *
 from test.datastructure.list.TestQ3 import *
+import unittest
 
 if __name__ == '__main__':
-    suite1 = unittest.TestLoader().loadTestsFromTestCase(TestQ1)
-    suite2 = unittest.TestLoader().loadTestsFromTestCase(TestQ2)
-    suite3 = unittest.TestLoader().loadTestsFromTestCase(TestQ3)
-    suite4 = unittest.TestLoader().loadTestsFromTestCase(TestQ4)
-    suite5 = unittest.TestLoader().loadTestsFromTestCase(TestQ5)
-    alltests = unittest.TestSuite([suite1, suite2, suite3, suite4, suite5])
-    runner=unittest.TextTestRunner()
-    runner.run(alltests)
+    unittest.TestLoader().loadTestsFromTestCase(TestQ1)
+    unittest.TestLoader().loadTestsFromTestCase(TestQ2)
+    unittest.TestLoader().loadTestsFromTestCase(TestQ3)
+    unittest.TestLoader().loadTestsFromTestCase(TestQ4)
+    unittest.TestLoader().loadTestsFromTestCase(TestQ5)
+    unittest.main()
+
