7cb3c177e727bcd23a987990da3f382c1f26b057
Rui <manoel.rui@5sdigital.com>
2016-03-26 11:28:37 -0300

    Implementado CircularDoublyLinkedList e testes

---
+++ datastructure/list/CircularDoublyLinkedList.py
@@ -0,0 +1,67 @@
+from datastructure.list.CircularLinkedList import *
+
+
+class CircularDoublyLinkedList(CircularLinkedList):
+    def add(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            self.tail.prev = self.head
+        else:
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
+
+        self.head.prev = self.tail
+        self.tail.next = self.head
+        self.length += 1
+
+    def remove(self, value):
+        father, node_to_remove = self.find_node(value)
+
+        if node_to_remove is None:
+            return None
+
+        if node_to_remove == self.head:
+            self.head = node_to_remove.next
+        else:
+            if node_to_remove is self.tail:
+                self.tail = father
+            father.next = node_to_remove.next
+            if node_to_remove.next is not None:
+                node_to_remove.next.prev = father
+
+        self.head.prev = self.tail
+        self.tail.next = self.head
+        del node_to_remove
+        self.length -= 1
+
+    def remove_recursive(self, value):
+        return self.__removeRecursive(None, self.head, value)
+
+    def __removeRecursive(self, father, node, value):
+        if node is None:
+            return
+
+        if node.value == value:
+            if node == self.head:
+                self.head = node.next
+            else:
+                if node is self.tail:
+                    self.tail = father
+                father.next = node.next
+                if node.next is not None:
+                    node.next.prev = father
+
+            self.head.prev = self.tail
+            self.tail.next = self.head
+            del node
+            self.length -= 1
+            return
+
+        if node is self.tail:
+            return
+
+        father = node
+        self.__removeRecursive(father, node.next, value)
