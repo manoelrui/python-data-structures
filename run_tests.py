e796bf3f77f5a1bd554c1747fbbe3d19768bf1e5
Rui <manoel.rui@5sdigital.com>
2016-03-27 12:26:19 -0300

    Criado arquivo para executar todos os testes unitários

---
+++ run_tests.py
@@ -0,0 +1,16 @@
+import unittest
+from test.application.TestQ6 import *
+from test.datastructure.list.TestQ1 import *
+from test.datastructure.list.TestQ2 import *
+from test.datastructure.list.TestQ4 import *
+from test.datastructure.list.TestQ5 import *
+from test.datastructure.list.TestQ3 import *
+
+if __name__ == '__main__':
+    unittest.TestLoader().loadTestsFromTestCase(TestQ1)
+    unittest.TestLoader().loadTestsFromTestCase(TestQ2)
+    unittest.TestLoader().loadTestsFromTestCase(TestQ3)
+    unittest.TestLoader().loadTestsFromTestCase(TestQ4)
+    unittest.TestLoader().loadTestsFromTestCase(TestQ5)
+    unittest.TestLoader().loadTestsFromTestCase(TestQ6)
+    unittest.main()
---
+++ test/application/run.py
@@ -0,0 +1,7 @@
+import unittest
+from test.application.TestQ6 import *
+
+if __name__ == '__main__':
+    unittest.TestLoader().loadTestsFromTestCase(TestQ6)
+    unittest.main()
+
