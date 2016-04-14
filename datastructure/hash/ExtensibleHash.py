c903990e3b517956ee7b9db503adc32c88bb704c
Rui <manoel.rui@5sdigital.com>
2016-04-14 00:15:51 -0300

    Implementado inserção na ExtensibleHash

--- datastructure/hash/ExtensibleHash.py
+++ datastructure/hash/ExtensibleHash.py
@@ -4,22 +4,22 @@ from datastructure.hash.Hash import *
 
 class Bucket(object):
     def __init__(self, bucket_len):
-        self.deep = 1
-        self.vec = [None] * bucket_len
+        self.deep = 0
+        self.container = [None] * bucket_len
         self.length = 0
 
     def __len__(self):
-        total_length = 0
-        for element in self.vec:
-            if element is not None:
-                total_length += 1
-        return total_length
+        return self.length
 
     def is_full(self):
-        return len(self) >= len(self.vec)
+        return len(self) >= len(self.container)
 
     def add(self, key, value):
-        pass
+        if self.is_full():
+            return False
+        self.container[self.length] = (HashTuple(key, value))
+        self.length += 1
+        return True
 
     def find(self):
         pass
@@ -32,28 +32,53 @@ class ExtensibleHash(Bucket):
     def __init__(self, n, bucket_len):
         super(ExtensibleHash, self).__init__(bucket_len)
         self._n_bits = 32
-        self.deep = math.ceil(math.log(n, 2))
+        self.deep = int(math.ceil(math.log(n, 2)))
         self.number_of_buckets = 1
         self.bucket_len = bucket_len
-        self.vec = [Bucket(bucket_len)] * int(math.pow(2, self.deep))
-        self.func = lambda key: mod((key * 101), math.pow(2, self._n_bits))
+        self.container = [Bucket(bucket_len)] * int(math.pow(2, self.deep))
+        self.func = lambda key: int(mod((key * 101), math.pow(2, self._n_bits)))
 
     def __len__(self):
         total_length = 0
-        return total_length
+        return len(s)
 
     def get_bucket(self, key):
         pass
 
     def add(self, key, value):
         hash_value = self.func(key)
-        bucket = self.vec[hash_value]
-        if len(bucket) < self.bucket_len:
-            bucket.add(HashTuple(key, value))
-        #split the bucket
+        bucket = self.container[hash_value & ((1 << self.deep) -1)]
+
+        if bucket.is_full() and bucket.deep >= self.deep:
+            return False
+            # TODO: To expand the key space
+
+        # Split one bucket in another two
+        if bucket.is_full() and bucket.deep < self.deep:
+            new_bucket_1 = Bucket()
+            new_bucket_2 = Bucket()
+
+            # split values in two buckets
+            for hash_tuple in Bucket:
+                hash_value = self.func(hash_tuple.key)
+                hash_value &= ((1 << self.deep) -1)
+                if (hash_value >> bucket.deep) & 1 == 1:
+                    new_bucket_1.add(key, value)
+                else:
+                    new_bucket_2.add(key, value)
+
+            # replace pointers to new buckets
+            for index, b in enumerate(self.container):
+                if b == bucket:
+                    if (index >> bucket.deep) & 1 == 1:
+                        self.container[index] = new_bucket_1
+                    else:
+                        self.container[index] = new_bucket_2
+            new_bucket_1.deep = new_bucket_2 = bucket.deep + 1
+            del bucket
+            return self.add(key, value)
         else:
-            if self.number_of_buckets < self.deep:
-                pass
+            return bucket.add(key, value)
 
     def find(self, key, value):
         pass
